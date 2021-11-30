a25b8bd641a5bb2458cdaef714ea3eaabcbd12db
daodat2000 <daodat2000@gmail.com>
2021-11-30 23:29:01 +0700

    delete and edit Grade structure

--- client/src/components/course/CourseGradeStructure.js
+++ client/src/components/course/CourseGradeStructure.js
@@ -1,37 +1,39 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import './CourseGradeStructure.css';
+import { Button, Input } from 'semantic-ui-react';
+
 const sampleScoreColumn = [
   {
     id: '1',
     GradeTitle: 'Exercise 1',
-    GradeDetail: '10%'
+    GradeDetail: '10%',
   },
   {
     id: '2',
     GradeTitle: 'Exercise 2',
-    GradeDetail: '10%'
+    GradeDetail: '10%',
   },
   {
     id: '3',
     GradeTitle: 'Midterm',
-    GradeDetail: '30%'
+    GradeDetail: '30%',
   },
   {
     id: '4',
     GradeTitle: 'Seminar',
-    GradeDetail: '10%'
+    GradeDetail: '10%',
   },
   {
     id: '5',
     GradeTitle: 'Final',
-    GradeDetail: '40%'
-  }
-]
+    GradeDetail: '40%',
+  },
+];
 
 export const CourseGradeStructure = (props) => {
   const [scoreColumn, updateScoreColumn] = useState(sampleScoreColumn);
-
+  const [editIndex, setEditIndex] = useState(1);
   function handleOnDragEnd(result) {
     if (!result.destination) return;
 
@@ -41,40 +43,86 @@ export const CourseGradeStructure = (props) => {
 
     updateScoreColumn(items);
   }
+  const DeleteGradeStructure = (index) => {
+    console.log(index);
+    const items = Array.from(scoreColumn);
+    items.splice(index, 1);
 
+    updateScoreColumn(items);
+  };
+  const EditGradeStructure = (index) => {
+    console.log(index);
+    setEditIndex(index);
+  };
+  const SaveGradeStructure = (index) => {
+    console.log(index);
+    setEditIndex(-1);
+  };
   return (
     <div>
-        <h1>Grade Structure</h1>
-        <DragDropContext onDragEnd={handleOnDragEnd}>
-          <Droppable droppableId="scoreColumn">
-            {(provided) => (
-              <ul className="scoreColumn" {...provided.droppableProps} ref={provided.innerRef}>
-                {scoreColumn.map(({id, GradeTitle, GradeDetail}, index) => {
-                  return (
-                    <Draggable key={id} draggableId={id} index={index}>
-                      {(provided) => (
-                        <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-                            {/* <div className="scoreColumn-thumb">
+      <h1>Grade Structure</h1>
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+        <Droppable droppableId='scoreColumn'>
+          {(provided) => (
+            <ul
+              className='scoreColumn'
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+              {scoreColumn.map(({ id, GradeTitle, GradeDetail }, index) => {
+                return (
+                  <Draggable key={id} draggableId={id} index={index}>
+                    {(provided) => (
+                      <li
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                      >
+                        {/* <div className="scoreColumn-thumb">
                               <img src={thumb} alt={`${name} Thumb`} />
                             </div> */}
-                          <p>
-                      
-                          { GradeTitle }
-                           
-                          </p>
-                          <p>
-                          {GradeDetail}
-                          </p>
-                        </li>
-                      )}
-                    </Draggable>
-                  );
-                })}
-                {provided.placeholder}
-              </ul>
-            )}
-          </Droppable>
-        </DragDropContext>
+                        {/* <p>{GradeTitle}</p> */}
+                        <Input
+                          label='Grade Title'
+                          placeholder='Grade Title'
+                          value={GradeTitle}
+                          disabled={editIndex !== index}
+                        />
+                        <Input
+                          label='Grade Detail'
+                          placeholder='Grade Detail'
+                          value={GradeDetail}
+                          disabled={editIndex !== index}
+                        />
+                        {/* <p>{GradeDetail}</p> */}
+                        <Button.Group>
+                          <Button onClick={() => DeleteGradeStructure(index)}>
+                            Delete
+                          </Button>
+                          <Button.Or />
+                          {editIndex !== index ? (
+                            <Button onClick={() => EditGradeStructure(index)}>
+                              Edit
+                            </Button>
+                          ) : (
+                            <Button
+                              positive
+                              onClick={() => SaveGradeStructure(index)}
+                            >
+                              Save
+                            </Button>
+                          )}
+                        </Button.Group>
+                      </li>
+                    )}
+                  </Draggable>
+                );
+              })}
+              {provided.placeholder}
+            </ul>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
-}
+};
